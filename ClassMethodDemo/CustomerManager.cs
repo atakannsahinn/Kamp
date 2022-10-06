15a2047b622a2bb6801235ef1313457d84e19d7a
atakannsahinn <atakannsahinn@hotmail.com>
2022-10-06 21:54:59 +0300

    C# 3.hafta ödevi yapıldı

---
+++ ClassMethodDemo/CustomerManager.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMethodDemo
+{
+    internal class CustomerManager
+    {
+        
+        public void AddCustomer(Customer customer, List<Customer> customers)
+        {
+            customers.Add(customer);
+        }
+
+        public void DeleteCustomer(Customer customer, List<Customer> customers) 
+        {
+            customers.Remove(customer);
+        }
+
+        public void ShowCustomers(List<Customer> customers)
+        {
+            foreach (var customer in customers)
+            {
+                Console.WriteLine("Customer Id : " + customer.CustomerId);
+                Console.WriteLine("Customer Name : " + customer.CustomerName);
+                Console.WriteLine("Customer No : " + customer.CustomerNo);
+                Console.WriteLine("Customer Balance : " + customer.CustomerBalance);
+                Console.WriteLine("*********************************");
+
+            }
+        }
+
+        public void DepositMoney(Customer customer, double money) 
+        {
+            customer.CustomerBalance = customer.CustomerBalance + money;
+        }
+
+        public void WithdrawMoney(Customer customer, double money)
+        {
+            customer.CustomerBalance = customer.CustomerBalance - money;
+        }
+    }
+}
---
+++ ClassMethodDemo/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace ClassMethodDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var customers = new List<Customer> (); 
+
+            Customer customer1 = new Customer();
+            customer1.CustomerName = "Atakan";
+            customer1.CustomerId = 1;
+            customer1.CustomerNo = "1258";
+            customer1.CustomerBalance = 12580.90;
+
+            Customer customer2 = new Customer();
+            customer2.CustomerName = "İlayda";
+            customer2.CustomerId = 2;
+            customer2.CustomerNo = "2688";
+            customer2.CustomerBalance = 569;
+
+            Customer customer3 = new Customer();
+            customer3.CustomerName = "Hasan";
+            customer3.CustomerId = 3;
+            customer3.CustomerNo = "6518";
+            customer3.CustomerBalance = 3689.98;
+
+            CustomerManager customerManager = new CustomerManager();
+            customerManager.AddCustomer(customer1, customers);
+            customerManager.AddCustomer(customer2, customers);
+            customerManager.AddCustomer(customer3, customers);
+
+            customerManager.DeleteCustomer(customer2, customers);
+
+            customerManager.DepositMoney(customer1, 1200);
+
+            customerManager.WithdrawMoney(customer3, 650);
+            
+            customerManager.ShowCustomers(customers);
+
+        }
+    }
+}
