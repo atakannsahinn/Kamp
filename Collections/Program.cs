eb4bd6263ceda52f2d59868020f4dce6ef0553e2
atakannsahinn <atakannsahinn@hotmail.com>
2022-10-07 00:57:51 +0300

    C# 4.gün ders sonu

---
+++ Collections/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace Collections
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] isimler = new string[] { "atakan", "engin", "ilker" };
+            //isimler[3] = "ilayda"; // arrayler sonradan değiştirilemez o yüzden bu satırda hata alırız.
+
+            // Bu yüzden collectionslar yani listeleri kullanırız.
+
+            List<string> isimler2 = new List<string>(); // boş liste
+
+            List<string> isimler3 = new List<string> {"atakan", "engin", "ilker"};
+            Console.WriteLine(isimler3[0]);
+            Console.WriteLine(isimler3[1]);
+            Console.WriteLine(isimler3[2]);
+            
+            isimler3.Add("ilayda");
+
+            Console.WriteLine("\n\n");
+
+            Console.WriteLine(isimler3[3]);
+            Console.WriteLine(isimler3[0]);
+
+
+
+        }
+    }
+}
---
+++ GenericsIntro/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace GenericsIntro
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            MyList<string> names = new MyList<string>();
+            names.Add("a");
+            names.Add("b");
+            Console.WriteLine(names);
+        }
+    }
+}
