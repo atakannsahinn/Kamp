1615df8ed994154c6860a87f99a2b0f78be2d640
atakannsahinn <atakannsahinn@hotmail.com>
2022-10-07 19:09:22 +0300

    4. hafta ödevi bitti

--- GenericsIntro/Program.cs
+++ GenericsIntro/Program.cs
@@ -5,9 +5,7 @@
         static void Main(string[] args)
         {
             MyList<string> names = new MyList<string>();
-            names.Add("a");
-            names.Add("b");
-            Console.WriteLine(names);
+            
         }
     }
 }
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.VisualBasic;
+
+namespace MyDictionary
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, int> dictionary = new Dictionary<string, int>();
+            dictionary.Add("istanbul", 34);
+            dictionary.Add("ankara", 06);
+            dictionary.Add("adana", 01);
+            dictionary.Add("izmir", 35);
+
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine(item);
+            }
+
+            MyDictionary<string, int> dictionary2 = new MyDictionary<string, int>();
+            dictionary2.Add("eskişehir", 26);
+            dictionary2.Add("konya", 42);
+            dictionary2.Show();
+            
+        }
+    }
+
+    class MyDictionary<T1, T2>
+    {
+        KeyValuePair<T1, T2>[] dictionary;
+
+        public MyDictionary()
+        {
+            dictionary = new KeyValuePair<T1, T2>[0];
+        }
+
+        public void Add(T1 key, T2 value)
+        {
+            KeyValuePair<T1, T2>[] _tempDict =  dictionary;
+             dictionary= new KeyValuePair<T1, T2>[dictionary.Length +1];
+            for (int i = 0; i < _tempDict.Length; i++)
+            {
+                dictionary[i] = _tempDict[i];
+            }
+            dictionary[dictionary.Length - 1] = new KeyValuePair<T1, T2>(key, value);
+            
+        }
+
+        public void Show()
+        {
+            foreach (var item in dictionary)
+            {
+                Console.WriteLine("[" + item.Key + ", " + item.Value + "]");
+            }
+        }
+    }
+
+   
+}
